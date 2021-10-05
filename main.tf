5acdcb6015696444531b5de54d29938bd14a94e2
gamre20 <mamadi.noname@gmail.com>
2021-10-04 20:57:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    openshift = {
+      source = "llomgui/openshift"
+      version = "1.1.0"
+    }
+  }
+}
 
+provider "openshift" {
+  # Configuration options
+}
