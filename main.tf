22b0aff048eacd84a633b7038302d79c1dd086c6
gamre20 <mamadi.noname@gmail.com>
2021-10-06 14:19:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,32 @@
+terraform {
+  required_providers {
+   nutanix = {
+      source = "nutanix/nutanix"
+
+   }
+  
+  }
+
+}
+
+provider "nutanix" {
+  username = var.nutanix_username
+  password = var.nutanix_passord
+  endpoint = var.nutanix_endpoint
+  port = var.nutanix_port
+  insecure = true
+  wait_timeout = 10
+  session_auth = true
+}
+
 data "nutanix_clusters" "clusters" {}
 
 resource "nutanix_virtual_machine" "vm1" {
-  name = "test-dou"
+
+  name = "test-vm"
   cluster_uuid = data.nutanix_clusters.clusters.entities.0.metadata.uuid
-
-  categories {
-        name   = "Environment"
-    value  = "Staging"
-    }
-
-
   num_vcpus_per_socket = 1
   num_sockets          = 1
   memory_size_mib      = 2048
+  
 }
