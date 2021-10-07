c280c5552e48d66419e2f377801b0416bc62cab5
gamre20 <mamadi.noname@gmail.com>
2021-10-07 14:42:54 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+   nutanix = {
+      source = "nutanix/nutanix"
+
+   }
+
+  }
+
+}
+
+provider "nutanix" {
+  username = var.nutanix_username
+  password = var.nutanix_passord
+  endpoint = var.nutanix_endpoint
+  port = var.nutanix_port
+  insecure = true
+  wait_timeout = 10
+  session_auth = true
+}
+
+data "nutanix_clusters" "clusters" {}
+
+resource "nutanix_virtual_machine" "vm1" {
+
+  name = "test-vm"
+  cluster_uuid = data.nutanix_clusters.clusters.entities.0.metadata.uuid
+  num_vcpus_per_socket = 1
+  num_sockets          = 1
+  memory_size_mib      = 2048
+
+}
