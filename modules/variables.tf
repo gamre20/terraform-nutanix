bb665e4f2ab03b8178227a780e4f9ca4abc5a4e9
gamre20 <mamadi.noname@gmail.com>
2021-10-08 17:18:10 -0400

    Update and rename variables.tf to modules/variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,21 +1,26 @@
 variable "nutanix_username" {
   description = "username"
-  default     = ""
+  default     = "admin"
 }
 
 variable "nutanix_passord" {
   description = "password"
-  default     = ""
+  default     = "nx2Tech291!"
 }
 
 variable "nutanix_endpoint" {
-  default     = ""
+  default     = "10.38.62.37"
 }
 
 variable "nutanix_port" {
-  default     = ""
+  default     = "9440"
 }
 
 variable "nutanix_vnic" {
   default     = "de87ab87-4ee1-4921-9064-1139c33937c7"
+}
+
+variable "vm_name" {
+  description = "VM's Names"
+  type = string
 }
