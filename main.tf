1ff4e45a95fac5f1f5557975a5ae0fd690e3952b
gamre20 <mamadi.noname@gmail.com>
2021-10-12 07:20:59 -0700

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,26 +1,37 @@
 terraform {
-  nutanix = {
-      source = "nutanix/nutanix"
-   }
+    required_version = ">= 0.12"
 }
 
-provider "nutanix" {
-  username        = var.nutanix_username
-  password        = var.nutanix_passord
-  endpoint        = var.nutanix_endpoint
-  port            = var.nutanix_port
-  insecure        = true
-  wait_timeout    = 10
-  session_auth    = true
-}
+data "nutanix_clusters" "ocp-cluster" {}
 
-resource "nutanix_image" "rhcos_image" {
-  name            = "rhcos"
-  description     = "Red Hat CoreOs"
-  source_uri      = "http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
-}
 
-module "create_vm" {
-  source = "./modules/vm"
-  
+resource "nutanix_virtual_machine" "ocp_nodes" {
+
+  name                 = var.vm_name
+  cluster_uuid         = data.nutanix_clusters.clusters.entities.0.metadata.uuid
+  num_vcpus_per_socket = "4"
+  num_sockets          = "2"
+  memory_size_mib      = 65536
+
+  disk_list {
+    data_source_reference = {
+      kind = "image"
+      uuid = nutanix_image.rhcos_image.id
+    }
+  }
+
+  disk_list {
+    disk_size_mib = 122880
+    device_properties {
+      device_type = "DISK"
+      disk_address = {
+        "adapter_type" = "SCSI"
+        "device_index" = "1"
+      }
+    }
+  }
+
+  nic_list {
+    subnet_uuid = data.nutanix_subnet.subnet.metadata.uuid
+  }
 }
